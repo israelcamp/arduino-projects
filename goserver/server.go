5c014e4e0edc0336da3fd99cea3183676fa8a98a
Israel Campiotti <israelcampiotti@gmail.com>
2025-07-16 20:47:13 -0300

    refactoring

--- goserver/server.go
+++ goserver/server.go
@@ -6,17 +6,41 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
+type Config struct {
+	Esp32Cam struct {
+		URL             string `yaml:"url"`
+		CaptureEndpoint string `yaml:"capture"`
+	} `yaml:"esp32cam"`
+	FileSystem struct {
+		ImagesDir string `yaml:"imagesDir"`
+	} `yaml:"fileSystem"`
+}
+
 var currentImagePath string
+
+func readConfig() Config {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		panic(err)
+	}
+	return cfg
+}
 
 func getNowFormated() string {
 	now := time.Now()
 	return fmt.Sprintf("%d-%d-%d_%d-%d-%d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 }
 
-func capture() {
-	resp, err := http.Get("http://192.168.0.107/capture")
+func capture(endpoint string, imagesDir string) {
+	resp, err := http.Get(endpoint)
 	if err != nil {
 		panic(err)
 	}
@@ -25,7 +49,7 @@ func capture() {
 	fmt.Println("Status", resp.Status)
 	nowString := getNowFormated()
 
-	currentImagePath = fmt.Sprintf("images/%s.png", nowString)
+	currentImagePath = fmt.Sprintf("%s/%s.png", imagesDir, nowString)
 	out, err := os.Create(currentImagePath)
 	if err != nil {
 		panic(err)
@@ -39,12 +63,12 @@ func capture() {
 	fmt.Println("Image saved")
 }
 
-func keepCapturing() {
+func keepCapturing(cfg Config) {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		capture()
+		capture(fmt.Sprintf("%s%s", cfg.Esp32Cam.URL, cfg.Esp32Cam.CaptureEndpoint), cfg.FileSystem.ImagesDir)
 	}
 }
 
@@ -59,7 +83,9 @@ func serveCapture(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	go keepCapturing()
+	config := readConfig()
+
+	go keepCapturing(config)
 
 	http.HandleFunc("/capture", serveCapture)
 
